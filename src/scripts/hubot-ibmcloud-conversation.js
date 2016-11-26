2b4ae8f3ba044df68f6c34bc11812ce798bf4fbb
de Lignières <tanguy.de.lignieres@accenture.com>
2016-11-26 10:39:33 +0100

    create a channel per project

--- src/scripts/hubot-ibmcloud-conversation.js
+++ src/scripts/hubot-ibmcloud-conversation.js
@@ -410,7 +410,9 @@ module.exports = function(robotAdapter) {
 
 					robot.logger.debug("Request body for channels.join : " + reqbody);
 
-					robot.http("https://slack.com/api/channels.join").header("Content-Type", "application/json").post(reqbody)(function(err, res, body) {
+					robot.http("https://slack.com/api/channels.join?token=process.env.HUBOT_SLACK_USER_TOKEN&name=" + encodeURIComponent(channelName))
+						.header("Content-Type", "application/json")
+						.post(JSON.stringify({}))(function(err, res, body) {
 					  if (res.statusCode === 200) {
 					  	robot.logger.debug("Response body of channels.join : " + body);
 					    return;
